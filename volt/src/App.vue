abaaa4a1dffb9dffc07072a8884dd38edd4b8229
Jeffrey <piccirilloj1@gmail.com>
2020-10-10 13:14:01 -0500

    Small commit to test how many extra contributions are created for this deployemnt

--- volt/src/App.vue
+++ volt/src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     Here are your environment vars: {{ env.VUE_APP_FIREBASE_AUTH_TOKEN }}
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
   data() {
     return {
       env: process.env,
