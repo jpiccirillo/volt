fa87011ad8df45f5ffb26187b4cfd58eb7f35b7a
Jeffrey <piccirilloj1@gmail.com>
2020-10-10 13:03:00 -0500

    Test using environment vars
    - Create .env file and gitignore it
    - Use process.env somewhere in application to show it is resolved to actual variable

--- volt/src/App.vue
+++ volt/src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    Here are your environment vars: {{ env.VUE_APP_FIREBASE_AUTH_TOKEN }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      env: process.env,
+    };
+  },
+};
 </script>
 
 <style>
